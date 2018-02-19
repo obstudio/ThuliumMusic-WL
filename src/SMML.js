5c7bb31fb2594983918cf9788f1ed6e4a0dd45b8
Shigma <33423008+Shigma@users.noreply.github.com>
2018-02-19 17:17:30 +0800

    Authorize standard package
    会不会有一种钦定的感觉？

--- src/SMML.js
+++ src/SMML.js
@@ -1,9 +1,9 @@
 const Parser = require('./Parser')
-const MMAAdapter = require('./adapter/MMAAdapter')
-const MIDIAdapter = require('./adapter/MIDIAdapter')
 
-module.exports = {
-    Parser,
-    MMAAdapter,
-    MIDIAdapter
+Error.prototype.toJSON = function () {
+    return {
+        message: this.message
+    }
 }
+
+module.exports = { Parser }

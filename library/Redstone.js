965e414fca1c597e2518ed53f58e13b4edef4213
Shigma <1700011071@pku.edu.cn>
2018-05-27 19:33:25 +0800

    add some optimizations
    1. 优化了mcfunction的代码量
    2. 缩小了生成的体积

--- library/Redstone.js
+++ library/Redstone.js
@@ -3,12 +3,26 @@ const Thulium = require('./Thulium')
 const Adapter = require('./Adapter/Adapter')
 const instrDict = require('./Config/Instrument.json')
 
+function setblock(x, y, z, block = 'redstone_wire', info = '') {
+  return `
+    setblock ~${x} ${y - 1} ~${z} stone_slab 8
+    setblock ~${x} ${y} ~${z} ${block} ${info}`
+}
+
+function setcommand(x, y, z, data, inst) {
+  return data.map((note, index) => `
+    setblock ~${x} ${y + index} ~${z} ${index === 0 ? '' : 'chain_'}command_block 1
+    blockdata ~${x} ${y + index} ~${z} {Command: "playsound ${inst}.${60 + note.Pitch} master @p ~ ~ ~ ${note.Volume}"}`
+  ).join('')
+}
+
 class TmRedstone {
   constructor({source, dest, spec = []}) {
     this.name = source.match(/\w+$/)[0]
     this.src = new Thulium(__dirname + '/../Songs/' + source + '.tm');
     this.data = new Adapter(this.src.parse(), ...spec).adapt()
-    this.dir = dest + '/data/functions/tm/' + this.name
+    this.dir = dest + '/data/functions/' + this.name + '/'
+    if (!fs.existsSync(this.dir)) fs.mkdirSync(this.dir)
   }
 
   generate() {
@@ -16,30 +30,27 @@ class TmRedstone {
       # Thulium Music for Minecraft 1.0
       # ${this.src.Comment.join('\n # ')}\n`
     output += this.data.map((data, index) => {
-      const y0 = 4 * Math.floor(index / 2) + 4
-      const result = index % 2 === 0
-        ? this.generateTrack(data, index, 1, 2, y0, 1)
-        : this.generateTrack(data, index, -1, 2, y0, 1)
-      return `
-        function tm:${this.name}_${index}
-        setblock ~-2 ${y0 - 1} ~-2 stone_slab 8
-        setblock ~-2 ${y0} ~-2 redstone_wire
-        setblock ~2 ${y0 - 1} ~-2 stone_slab 8
-        setblock ~2 ${y0} ~-2 redstone_wire 
-        ${y0 === 4 ? '' : `
-          setblock ~-1 ${y0 - 2} ~-2 stone_slab 8
-          setblock ~-1 ${y0 - 1} ~-2 redstone_wire
-          setblock ~1 ${y0 - 2} ~-2 stone_slab 8
-          setblock ~1 ${y0 - 1} ~-2 redstone_wire
-          setblock ~ ${y0 - 3} ~-2 stone_slab 8
-          setblock ~ ${y0 - 2} ~-2 redstone_wire
-          setblock ~-1 ${y0 - 4} ~-2 stone_slab 8
-          setblock ~-1 ${y0 - 3} ~-2 redstone_wire
-          setblock ~1 ${y0 - 4} ~-2 stone_slab 8
-          setblock ~1 ${y0 - 3} ~-2 redstone_wire
-        `}`
-    }).join('')
-    output += `
+      const z0 = Math.floor(index % 4 / 2)
+      const y0 = 4 * Math.floor(index / 4) + 4 + z0 * 2
+      this.generateTrack(data, index, {
+        d: 1 - 2 * (index % 2), 
+        x0: 2 + z0,
+        y0: y0,
+        z0: z0,
+        dv: 1
+      })
+      let result = `\nfunction ${this.name}:track_${index}`
+      if (y0 !== 4 && z0 === 0) {
+        result += setblock(-3, y0 - 1, -2)
+          + setblock(3, y0 - 1, -2)
+          + setblock(-2, y0 - 2, -2)
+          + setblock(2, y0 - 2, -2)
+          + setblock(-3, y0 - 3, -2)
+          + setblock(3, y0 - 3, -2)
+      }
+      return result
+    }).join('\n')
+    output += `\n
       setblock ~-2 4 ~-3 redstone_wire
       setblock ~-1 4 ~-3 redstone_wire
       setblock ~ 4 ~-3 redstone_wire
@@ -49,10 +60,10 @@ class TmRedstone {
       setblock ~ 4 ~-5 unpowered_repeater 14
       setblock ~ 4 ~-6 unpowered_repeater 14
       setblock ~ 4 ~-7 stone_button 5`
-    fs.writeFileSync(this.dir + '.mcfunction', output.replace(/^ +/gm, ''), {encoding: 'utf8'});
+    fs.writeFileSync(this.dir + 'build.mcfunction', output.replace(/^ +/gm, ''), {encoding: 'utf8'});
   }
 
-  generateTrack(track, id, d, x0, y0, dv) {
+  generateTrack(track, id, {d, x0, y0, z0, dv}) {
     const inst = track.Name.match(/\.(\w*)\./)[1]
     const instID = instrDict[inst] + 1
     const scale = track.Settings.Speed / 15
@@ -69,32 +80,33 @@ class TmRedstone {
     })
     let output = `\
       # Track: ${id}
-      # Instrument: ${inst}\n`
-    for (let i = 0; i < result.length; i++) {
-      const z = 2 * Math.floor(i / 4)
-      const x = d * (x0 + 2 * (i % 4))
-      output += i % 4 === 0
-        ? `
-          setblock ~${x} ${y0 - 1} ~${z - 1} stone_slab 8
-          setblock ~${x} ${y0} ~${z - 1} unpowered_repeater 14`
-        : `
-          setblock ~${x - d} ${y0 - 1} ~${z} stone_slab 8
-          setblock ~${x - d} ${y0} ~${z} unpowered_repeater ${2 - d}`
-      output += `\nsetblock ~${x} ${y0 - 1} ~${z} stone_slab 8`
+      # Instrument: ${inst}
+      ${setblock(x0 * d, y0, z0 - 2)}`
+    for (let i = 0; i < result.length; i += 4) {
+      let x = d * x0, y = y0, z = z0 + i / 2, s = 0
+      output += setblock(x, y, z - 1, 'unpowered_repeater', 14)
+      output += `\nsetblock ~${x} ${y - 1} ~${z} stone_slab 8`
       output += result[i].length === 0
-        ? `\nsetblock ~${x} ${y0} ~${z} redstone_wire`
-        : result[i].map((item, j) => `
-          setblock ~${x} ${y0 + j} ~${z} ${j === 0 ? '' : 'chain_'}command_block 1
-          blockdata ~${x} ${y0 + j} ~${z} {Command: "playsound ${instID}.${60 + item.Pitch} master @p ~ ~ ~ ${item.Volume}"}`
-        ).join('')
-      output += '\n'
+        ? `\nsetblock ~${x} ${y} ~${z} redstone_wire`
+        : setcommand(x, y, z, result[i], instID)
+
+      for (let j = 1; j < 4; j++) {
+        s += 1
+        if (result[i + j].length !== 0) {
+          x += 2 * d
+          output += setblock(x - d, y, z, 'unpowered_repeater', 4 * s - 2 - d)
+            + `\nsetblock ~${x} ${y - 1} ~${z} stone_slab 8`
+            + setcommand(x, y, z, result[i + j], instID)
+          s = 0
+        }
+      }
     }
-    fs.writeFileSync(this.dir + `_${id}.mcfunction`, output.replace(/^ +/gm, ''), {encoding: 'utf8'})
+    fs.writeFileSync(this.dir + `track_${id}.mcfunction`, output.replace(/^ +/gm, ''), {encoding: 'utf8'})
     return true
   }
 }
 
 new TmRedstone({
-  source: 'Touhou/test4',
+  source: 'Touhou/Oriental_Blood',
   dest: 'D:/Minecraft/.minecraft/saves/test'
 }).generate()
